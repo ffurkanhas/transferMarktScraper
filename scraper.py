473b2cd91053a9443dfad92b8006dda37d6c831d
Fatih Furkan Has <ffatihhas@gmail.com>
2018-07-12 00:11:47 +0300

    club traversing is completed and it is writing to teams.json

--- scraper.py
+++ scraper.py
@@ -6,8 +6,6 @@ from selenium.webdriver.common.keys import Keys
 from pynput.keyboard import Key, Controller
 baseUrl = 'https://www.transfermarkt.com'
 
-playersJson = open('players.json', 'a')
-teamsJson = open('teams.json', 'a')
 logTxt = open('log.txt', 'a')
 
 countryCompleted = False
@@ -38,7 +36,6 @@ def parser(countryStart, competitionStart, clubStart, playerStart):
     allCountriesList = driver.find_elements_by_class_name('active-result')
 
     for countryNumber in range(countryStart, len(allCountriesList)):
-        countryStart = 0
         country = allCountriesList[countryNumber]
         global countryBreakPoint
         countryBreakPoint = countryNumber
@@ -54,7 +51,6 @@ def parser(countryStart, competitionStart, clubStart, playerStart):
         allCompetitionListTable = responsiveDiv[0].find_elements_by_class_name('inline-table')
 
         for competitionNumber in range(competitionStart, len(allCompetitionListTable)):
-            competitionStart = 0
             global competitionBreakPoint
             competitionBreakPoint = competitionNumber
             allTdsForCompetitionLinks = allCompetitionListTable[competitionNumber].find_elements_by_tag_name("td")
@@ -62,37 +58,64 @@ def parser(countryStart, competitionStart, clubStart, playerStart):
             competitionName = allTdsForCompetitionLinks[1].text
             print("\t" + str(competitionName).strip('\n'))
             allTdsForCompetitionLinks[1].click()
+            competitionBreakPoint = 0
             allClubLinks = driver.find_elements_by_class_name('hauptlink.no-border-links.show-for-small.show-for-pad')
 
             for clubNumber in range(clubStart, len(allClubLinks)):
-                clubStart = 0
+                teamsJson = open('teams.json', 'a')
                 global clubBreakPoint
                 clubBreakPoint = clubNumber
                 clubNameAndLink = allClubLinks[clubNumber].find_element_by_class_name('vereinprofil_tooltip.tooltipstered')
                 clubName = clubNameAndLink.text
-                print("\t" + str(clubNumber) + ": " + clubName)
-                clubNameAndLink.click()
 
+                print("\t" + str(clubNumber) + ": " + clubName)
+                global clubCompleted
+
+                clubNameAndLink.click()
+                if clubCompleted is False:
+                    clubDict = {}
+                    clubDict['club_name'] = clubName
+                    clubDict['club_country'] = countryName
+                    clubDict['competition_name'] = competitionName
+                    clubDict['updated_date'] = str(datetime.now())
+
+                    clubValueField = driver.find_element_by_class_name('dataMarktwert')
+                    clubValue = clubValueField.find_element_by_css_selector('a').text
+                    clubValue += " " + clubValueField.find_element_by_class_name('waehrung').text
+                    clubDict['total_value'] = clubValue[:clubValue.index("€") + 1]
+
+                    json.dump(clubDict, teamsJson, indent=4, ensure_ascii=False)
+                    teamsJson.write(',')
+                    teamsJson.close()
+                clubCompleted = True
                 allPlayerLinks = driver.find_elements_by_class_name('posrela')
 
                 for playerNumber in range(playerStart, len(allPlayerLinks)):
-                    playerStart = 0
+                    playersJson = open('players.json', 'a')
                     global playerBreakPoint
                     playerBreakPoint = playerNumber
                     playerNameAndLink = allPlayerLinks[playerNumber].find_element_by_class_name('spielprofil_tooltip.tooltipstered')
                     playerName = playerNameAndLink.text
                     print("\t\t" + str(playerNumber) + ": " + playerName)
+
+                    playersJson.close()
                     playerNameAndLink.click()
 
                     #repeat steps for other players
+                    playerBreakPoint = 0
+                    playerStart = 0
                     driver.back()
                     allPlayerLinks = driver.find_elements_by_class_name('posrela')
 
+                clubCompleted = False
                 #repeat steps for other clubs
+                clubBreakPoint = 0
+                clubStart = 0
                 driver.back()
                 allClubLinks = driver.find_elements_by_class_name('hauptlink.no-border-links.show-for-small.show-for-pad')
 
             #repeat steps for other competitions
+            countryBreakPoint = 0
             driver.back()
             responsiveDiv = driver.find_elements_by_class_name('responsive-table')
             allCompetitionListTable = responsiveDiv[0].find_elements_by_class_name('inline-table')
@@ -115,11 +138,3 @@ if(__name__ == '__main__'):
             globals().get('driver').close()
             pass
     logTxt.close()
-    # allTdsForClubLinks = driver.find_elements_by_class_name('hauptlink no-border-links hide-for-small hide-for-pad')
-    #
-    # for clubNumber in range(0, len(allTdsForClubLinks)):
-    #     clubNameAndLink = allTdsForClubLinks[clubNumber].find_element_by_class_name(
-    #         'vereinprofil_tooltip tooltipstered')
-    #     clubName = clubNameAndLink.text
-    #     print(clubName)
-    #     clubNameAndLink.click()
