abe90356d99e3ce604d7256911ba9d5b5966c0d8
Fatih Furkan Has <ffatihhas@gmail.com>
2018-07-12 04:21:49 +0300

    something is good

--- scraper.py
+++ scraper.py
@@ -6,14 +6,14 @@ from selenium.webdriver.common.keys import Keys
 from pynput.keyboard import Key, Controller
 baseUrl = 'https://www.transfermarkt.com'
 
-logTxt = open('log.txt', 'a')
+
 
 countryCompleted = False
 competitionCompleted = False
 clubCompleted = False
 playerCompleted = False
 
-countryBreakPoint = 1
+countryBreakPoint = 0
 competitionBreakPoint = 0
 clubBreakPoint = 0
 playerBreakPoint = 0
@@ -58,17 +58,21 @@ def parser(countryStart, competitionStart, clubStart, playerStart):
             competitionName = allTdsForCompetitionLinks[1].text
             print("\t" + str(competitionName).strip('\n'))
             allTdsForCompetitionLinks[1].click()
+
             competitionBreakPoint = 0
             allClubLinks = driver.find_elements_by_class_name('hauptlink.no-border-links.show-for-small.show-for-pad')
 
             for clubNumber in range(clubStart, len(allClubLinks)):
                 teamsJson = open('teams.json', 'a')
+                keyboard.press(Key.down)
+                time.sleep(1)
+                keyboard.release(Key.down)
                 global clubBreakPoint
                 clubBreakPoint = clubNumber
                 clubNameAndLink = allClubLinks[clubNumber].find_element_by_class_name('vereinprofil_tooltip.tooltipstered')
                 clubName = clubNameAndLink.text
 
-                print("\t" + str(clubNumber) + ": " + clubName)
+                print("\t\t" + str(clubNumber) + ": " + clubName)
                 global clubCompleted
 
                 clubNameAndLink.click()
@@ -89,23 +93,64 @@ def parser(countryStart, competitionStart, clubStart, playerStart):
                     teamsJson.close()
                 clubCompleted = True
                 allPlayerLinks = driver.find_elements_by_class_name('posrela')
+                currentClubUrl = driver.current_url
 
                 for playerNumber in range(playerStart, len(allPlayerLinks)):
                     playersJson = open('players.json', 'a')
                     global playerBreakPoint
                     playerBreakPoint = playerNumber
-                    playerNameAndLink = allPlayerLinks[playerNumber].find_element_by_class_name('spielprofil_tooltip.tooltipstered')
-                    playerName = playerNameAndLink.text
-                    print("\t\t" + str(playerNumber) + ": " + playerName)
+                    playerClickFlag = False
+                    try:
+                        playerNameAndLink = allPlayerLinks[playerNumber].find_element_by_class_name('spielprofil_tooltip.tooltipstered')
+                        playerName = playerNameAndLink.text
+                        print("\t\t\t" + str(playerNumber) + ": " + playerName)
+                        playerNameAndLink.click()
+                        playerClickFlag = True
 
-                    playersJson.close()
-                    playerNameAndLink.click()
+                        player_stats_all = driver.find_elements_by_class_name('auflistung')
 
-                    #repeat steps for other players
+                        player_stats_main = player_stats_all[2]
+                        player_bdate = ""
+                        player_nation = ""
+                        player_position = ""
+
+                        for stat in player_stats_main.find_elements_by_tag_name('tr'):
+                            clean_info = stat.find_element_by_tag_name('td')
+
+                            if (stat.find_element_by_tag_name('th').text.strip() == 'Date of birth:'):
+                                player_bdate = clean_info.text.strip()
+                            elif (stat.find_element_by_tag_name('th').text.strip() == 'Nationality:'):
+                                player_nation = clean_info.text.strip()
+                            elif (stat.find_element_by_tag_name('th').text.strip() == 'Position:'):
+                                player_position = clean_info.text.strip()
+
+                        playerDict = {}
+                        playerDict['name'] = playerName
+                        playerDict['date_of_birth'] = player_bdate
+                        playerDict['nationality'] = player_nation
+                        playerDict['position'] = player_position
+                        playerDict['current_club'] = clubName
+                        playerDict['price'] = ""
+                        playerDict['updated_date'] = str(datetime.now())
+
+                        json.dump(playerDict, playersJson, indent=4, ensure_ascii=False)
+                        playersJson.write(',')
+                        playersJson.close()
+
+                    except:
+                        print("player parse error")
+
+                    if playerClickFlag is True:
+                        driver.get(currentClubUrl)
+                        time.sleep(1)
+                    allPlayerLinks = driver.find_elements_by_class_name('posrela')
                     playerBreakPoint = 0
                     playerStart = 0
-                    driver.back()
-                    allPlayerLinks = driver.find_elements_by_class_name('posrela')
+
+                    logTxt = open('log.txt', 'a')
+                    logTxt.write('\n')
+                    logTxt.write(':' + str(countryNumber) + "," + str(competitionNumber) + "," + str(clubNumber) + "," + str(playerNumber) + "\n")
+                    logTxt.close()
 
                 clubCompleted = False
                 #repeat steps for other clubs
@@ -121,14 +166,26 @@ def parser(countryStart, competitionStart, clubStart, playerStart):
             allCompetitionListTable = responsiveDiv[0].find_elements_by_class_name('inline-table')
 
         #repeat steps for other countries
-        logTxt.write('Country: ' + str(countryNumber) + "\n")
         countryDropDownMenu = driver.find_element_by_id('land_select_breadcrumb_chzn')
         countryDropDownMenu.click()
         time.sleep(1)
         allCountriesList = driver.find_elements_by_class_name('active-result')
 
 if(__name__ == '__main__'):
-    logTxt.write('\n----------------\n' + str(datetime.now()))
+    with open('log.txt', 'r') as f:
+        lines = f.read().splitlines()
+        last_line = lines[-1]
+        resumeNumbers = last_line[1:].split(',')
+
+    if len(resumeNumbers) == 4:
+        countryBreakPoint = int(resumeNumbers[0])
+        competitionBreakPoint = int(resumeNumbers[1])
+        clubBreakPoint = int(resumeNumbers[2])
+        playerBreakPoint = int(resumeNumbers[3])
+        countryCompleted = True
+        competitionCompleted = True
+        clubCompleted = True
+
     while True:
         try:
             parser(countryBreakPoint, competitionBreakPoint, clubBreakPoint, playerBreakPoint)
@@ -137,4 +194,3 @@ if(__name__ == '__main__'):
             print(str(e))
             globals().get('driver').close()
             pass
-    logTxt.close()
